68d0012b2f3e7b55890f5da7b8c86eb01d2e8bcf
Alphacreed <wynoral@gmail.com>
2023-07-03 16:29:17 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
+
 
 
 const app = express()
 
 //Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes Middleware
+app.use("/api/users",userRoute);
 
 //Routes
 app.get("/",(req,res)=>{
@@ -20,8 +29,11 @@ app.get("/",(req,res)=>{
 
 const PORT = process.env.PORT || 5000;
 
+//Error Middleware
+app.use(errorHandler);
+
 //Connect to mongoDb and start server
-mongoose
+mongoose 
     .connect(process.env.MONGO_URI)
     .then(()=>{
         app.listen(PORT,() =>{
